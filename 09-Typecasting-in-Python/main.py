8f481483e5ce6aee8b4a5e78d9ab079e0f708bcd
madhumitha01 <madhumitharajinikanthan@gmail.com>
2025-05-22 22:17:28 +0530

    Added Lecture 9

---
+++ 09-Typecasting-in-Python/main.py
@@ -0,0 +1,27 @@
+a = "1"
+# a = 1
+b = "2"
+# b = 2
+print(int(a) + int(b))
+
+# Implicit TypeCasting
+c = 1.9
+d = 8
+
+print(c + d)
+
+# Explicit Type Casting
+a = "15"
+print("Tyep of a is:", type(a))
+b = 7
+print("Type of b is:",type(b))
+
+c = int(a)
+
+print("Sum of a and b is:", c+b)
+
+#Implicit Type Casting
+a = 4.5
+b = 3
+c = a+b
+print("Sum = ", c)
