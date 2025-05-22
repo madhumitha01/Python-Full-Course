fea18e7c5f4182a62278e6713763066db5fb7fe4
madhumitha01 <madhumitharajinikanthan@gmail.com>
2025-05-22 22:17:44 +0530

    Added Lecture 10

---
+++ 10-Taking-User-Input/main.py
@@ -0,0 +1,17 @@
+a = input("Enter your name: ")
+print("My name is", a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print(type(x))
+print(type(y))
+print(x  + y)
+
+print(int(x) + int(y))
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(type(a))
+print(type(b))
+print(a  + b)
+
---
+++ 10-Taking-User-Input/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+a = input("Enter your name: ")
+# print("My name is", a)
+
+# x = input("Enter first number: ")
+# y = input("Enter second number: ")
+# print(type(x))
+# print(type(y))
+# print(x  + y)
+
+# print(int(x) + int(y))
